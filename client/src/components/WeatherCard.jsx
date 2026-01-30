9e44ffd55c3e326f4c5777f2d20c9ff7efafaad9
Kacie Dearman <kldearman@amarillocollege.com>
2026-01-29 23:28:47 -0600

    Removed import from WeatherCard.jsx.

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-
 function WeatherCard({ weatherData }) {
 
     return (
         <div className="weather-card">
             <div className="results">
-                
+
             </div>
         </div>
     )
