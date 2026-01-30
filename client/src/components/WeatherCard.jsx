8e110b9ff558166011d2478d05bba1085652ea34
Kacie Dearman <kldearman@amarillocollege.com>
2026-01-30 10:30:55 -0600

    Added most of the required weather data to WeatherCard.jsx. Still need to add icon.

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -3,12 +3,17 @@ function WeatherCard({ weatherData }) {
     return (
         <div className="weather-card">
             <div className="results">
-                <h2>Here's today's weather in {weatherData.name}</h2>
+                <h2>Here's the current weather in {weatherData.name}</h2>
                 //TODO:
                 // current weather icon
+                <p>Condition: {weatherData.weather.main}/p>
                 // temperature
+                <p>Temperature: {weatherData.main.temp}°</p>
+                <p>Description: {weatherData.weather[0].description}</p>
                 // humidity
+                <p>Humidity: {weatherData.main.humidity}%</p>
                 // wind speed
+                <p>Wind Speed: {weatherData.wind.speed} mph</p>
 
             </div>
         </div>
